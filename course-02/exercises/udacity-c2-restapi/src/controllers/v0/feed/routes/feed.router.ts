48d7bc5f70dbba48ee0fd77226d74afab16e090d
Selim Dursun <m.seldur@gmail.com>
2020-12-13 21:25:57 +0100

    two end points are implemented

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,43 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const {id} = req.params;
+    if (id) {
+        const item = await FeedItem.findByPk(id);
+        if (item) {
+            res.status(200).send(item);
+        } else {
+            return res.status(400).send("Item not found");
+        }
+    } else {
+        return res.status(400).send("Id required");
+    }
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        // res.send(500).send("not implemented")
+        const {id} = req.params;
+        const {caption} = req.body;
+        if (id && caption) {
+            const item = await FeedItem.findByPk(id);
+            if (item) {
+                item.set('caption', caption);
+                item.save().then(()=> {
+                    res.status(200).send("SUCCESS");
+                }).error((error)=> {
+                    res.send(400).send(error);
+                })
+            } else {
+                return res.status(400).send("Item or caption not found");
+            }
+        } else {
+            return res.status(400).send("Id and Caption(as body) required");
+        }
 });
 
 
